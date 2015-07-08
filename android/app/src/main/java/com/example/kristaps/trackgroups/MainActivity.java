f57587c47518eed376a6a20871104ec6938f1bad
KristapsVilks <kristaps.vilks@gmail.com>
2015-07-08 19:24:32 +0200

    Some stuff for DB

--- android/app/src/main/java/com/example/kristaps/trackgroups/MainActivity.java
+++ android/app/src/main/java/com/example/kristaps/trackgroups/MainActivity.java
@@ -1,11 +1,30 @@
 package com.example.kristaps.trackgroups;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.LoginFilter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.kristaps.trackgroups.R;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -37,4 +56,102 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    class login_task extends AsyncTask<String, String, Void>
+    {
+        private ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+            InputStream is = null;
+            String result = "";
+        protected void onPreExecute() {
+            ProgressDialog.setMessage("Loading data...");
+            progressDialog.show();
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                   task.this.cancel(true);
+                }
+            });
+
+        }
+
+        protected Void doInBackground(String... params) {
+            String url_select = "http://trackgroups.byethost18.com/trackGroups.php"
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url_select);
+
+            ArrayList<NameValuePair> param = new ArrayList<NameValuePair>();
+
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(param));
+
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+
+                is = httpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in http connection " + e.toString());
+                Toast.makeText(MainActivity.this, "Please Try Again", Toast.LENGTH_LONG).show();
+            }
+            try {
+                BuffferedReader br = new BufferReader(new InputStreanreader(is));
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n")
+                }
+                is.close();
+                result = sb.toString();
+
+            } catch (Exception e){
+                Log.e("log_tag", "Error converting result" + e.toString());
+            }
+            return null;
+        }
+
+        protected void OnPostExecute (void v) {
+            try {
+                JSONArray Jarray = new JSONArray(result);
+                for (int i=0; i<Jarray; i; i++)
+                {
+                    JSONObject Jasonobject = null;
+                    Jasonobject = Jarray.getJSONObject(i);
+
+                   // String name = JSONObject.getSting("name");
+                    String db_detail="";
+
+                    if(et.getText().toString().equalsIgnoreCase(name)){
+                      //  db_detail = Jaosnobject.getString("detail")
+                       // text.setText(db_detail)
+                        break;
+                    }
+
+                }
+                this.progressDialog.dismiss();
+
+            } catch (Exception e){
+                Log.e("log_tag", "Errro parsing data " + e.toString());
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
